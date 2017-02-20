07dff595e2b8c3219b56fd161477a90ef1c70ac3
sebastian.harko <sebastian.harko@lightbend.com>
2017-02-20 15:55:38 +0100

    send response to sender

--- src/main/scala/src/main/demo/Main.scala
+++ src/main/scala/src/main/demo/Main.scala
@@ -2,18 +2,19 @@ package src.main.demo
 
 import akka.actor.{ActorLogging, ActorSystem, PoisonPill, Props}
 import akka.persistence.{PersistentActor, RecoveryCompleted}
+import src.main.demo.ResponseDocument.NoResponseForSender
 
 sealed trait EventSourceable[T] {
 
   def applyEvent(t: T, e: Event): T
 
-  def applyCommand(t: T, c: Command): List[Event]
+  def applyCommand(t: T, c: Command): (ResponseDocument, List[Event])
 
   def applyEvents(t: T, events: List[Event]): T =
     events.foldLeft(t)((state, event) => applyEvent(state, event))
 
   def applyCommands(t: T, commands: List[Command]): T =
-    commands.foldLeft(t)((state: T, command: Command) => applyEvents(state, applyCommand(state, command)))
+    commands.foldLeft(t)((state: T, command: Command) => applyEvents(state, applyCommand(state, command)._2))
 
 }
 
@@ -23,6 +24,7 @@ object EventSourceableEntities {
 
     import Events._
     import Commands._
+    import ResponseDocument.NoResponseForSender
 
     def applyEvent(t: UserAccount, event: Event): UserAccount = {
 
@@ -61,42 +63,42 @@ object EventSourceableEntities {
         // normally, here we do some external API calls
         val validCreditCardPayment = true
         if (validCreditCardPayment)
-          List(PremiumAccountEnabled)
+          (NoResponseForSender, List(PremiumAccountEnabled))
         else
-          List()
+          (NoResponseForSender, List())
 
       case DisablePremiumAccount if !t.suspended =>
         if (t.accountType == Premium)
-          List(PremiumAccountDisabled)
+          (NoResponseForSender, List(PremiumAccountDisabled))
         else
-          List()
+          (NoResponseForSender, List())
 
       case UsePremiumFeature if !t.suspended && t.numActiveLogins > 0 =>
         if (t.accountType == Premium)
-          List(PremiumFeatureBeingUsed)
+          (NoResponseForSender, List(PremiumFeatureBeingUsed))
         else
-          List()
+          (NoResponseForSender, List())
 
       case LogIn(providedPassword) if !t.suspended =>
         if (providedPassword == t.password)
-          List(UserLoggedIn)
+          (NoResponseForSender, List(UserLoggedIn))
         else if (t.numFailedLoginsAttempts == 3)
         // cancel every active session
-          List.fill(t.numActiveLogins)(UserLoggedOut) :+ AccountSuspended
+          (NoResponseForSender, List.fill(t.numActiveLogins)(UserLoggedOut) :+ AccountSuspended)
         else
-          List(UserAuthFailed)
+          (NoResponseForSender, List(UserAuthFailed))
 
 
       case LogOut =>
         if (t.numActiveLogins > 0)
-          List(UserLoggedOut)
+          (NoResponseForSender, List(UserLoggedOut))
         else
-          List()
+          (NoResponseForSender, List())
 
       case ReActivateAccount(secretCode) if t.suspended && secretCode == "V6G1J1" =>
-        List(AccountReactivated)
+        (NoResponseForSender, List(AccountReactivated))
 
-      case _ => List()
+      case _ => (NoResponseForSender, List())
     }
 
   }
@@ -127,10 +129,15 @@ class AkkaPersistenceActorForEntity[T](entityId: String, initialEntityState: T)(
 
   override def receiveCommand: Receive = {
     case c: Command =>
-      val events = es.applyCommand(entityState, c)
+      val commandResult = es.applyCommand(entityState, c)
+      val response = commandResult._1
+      val events = commandResult._2
       persistAll(events) { (e: Event) =>
         log.info("persisting event {}", e.toString)
         entityState = es.applyEvent(entityState, e)
+        if (response != NoResponseForSender) {
+          sender ! response
+        }
       }
   }
 
@@ -138,9 +145,17 @@ class AkkaPersistenceActorForEntity[T](entityId: String, initialEntityState: T)(
 
 }
 
+sealed trait ResponseDocument
+
 sealed trait Command
 
 sealed trait Event
+
+object ResponseDocument {
+
+  case object NoResponseForSender extends ResponseDocument
+
+}
 
 object Commands {
 
@@ -187,8 +202,7 @@ case class UserAccount(accountType: AccountType = Free,
                        password: String = "default",
                        suspended: Boolean = false,
                        numActiveLogins: Int = 0,
-                       numFailedLoginsAttempts: Int = 0
-                    )
+                       numFailedLoginsAttempts: Int = 0)
 
 object Main extends App {
 
