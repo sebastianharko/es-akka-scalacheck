e41a549b85d33bd735dea8ea910473e8c0c867e4
sebastianharko <sebastian.harko@lightbend.com>
2018-11-12 11:32:40 -0500

    update versions, add a new test

--- build.sbt
+++ build.sbt
@@ -2,16 +2,15 @@ name := "Decouple"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.12.6"
 
 resolvers += Resolver.jcenterRepo
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.4.4",
-  "com.typesafe.akka" %% "akka-persistence" % "2.4.4",
+  "com.typesafe.akka" %% "akka-actor" % "2.5.7",
+  "com.typesafe.akka" %% "akka-persistence" % "2.5.7",
   "com.github.dnvriend" %% "akka-persistence-inmemory" % "2.4.17.1",
-  "org.scalatest" %% "scalatest" % "3.0.1" % "test",
-  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
-  "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.3"
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.14.0" % "test"
 )
 
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,24 +1,41 @@
 import org.scalacheck.Gen
-import org.scalacheck.Shapeless._
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 import src.main.demo.Commands._
 import src.main.demo.{Command, EventSourceableEntities, UserAccount}
 
+import org.scalacheck._
+import Gen._
+import Arbitrary.arbitrary
+
 class SimplePropertySpec extends PropSpec with PropertyChecks with Matchers {
 
-  val commandGen = Gen.containerOf[List, Command](Gen.oneOf(LogIn("default"), 
-    LogIn("123"), 
-    LogIn("password"), 
-    LogIn("bla")))
+  val logInGen = {
+    val randomPassword = for {
+      v ← arbitrary[String]
+    } yield LogIn(v)
+    val correctPassword = const(LogIn("default"))
+    Gen.nonEmptyListOf(Gen.oneOf(randomPassword, correctPassword))
+  }
 
-  val suspendedUser = UserAccount(suspended = true)
-
-  import EventSourceableEntities.{EsUserAccount => es}
+  import EventSourceableEntities.{EsUserAccount ⇒ es}
 
   property("absolutely no way to login if the account is suspended - even if password is correct") {
-    forAll(commandGen) { (commandList: List[Command]) =>
+    val suspendedUser = UserAccount(suspended = true)
+    forAll(logInGen) { commandList: List[Command] ⇒
       es.applyCommands(suspendedUser, commandList).numActiveLogins shouldBe 0
+    }
+  }
+
+  property("no way to login if the password is incorrect") {
+    val user = UserAccount(password = "summer200one", suspended = false)
+
+    val logInCommandWithRandomPass = for {
+      v ← arbitrary[String]
+    } yield LogIn(v)
+
+    forAll(logInCommandWithRandomPass) {
+      logIn ⇒ es.applyCommands(user, logIn :: Nil).numActiveLogins shouldBe 0
     }
   }
 
